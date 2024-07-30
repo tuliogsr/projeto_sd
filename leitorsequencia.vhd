ddda911b55fee8cea3c38cb4f22aca0c25b716d0
Tulio Gabriel <tuliogabrielsr@gmail.com>
2024-07-29 23:52:06 -0300

    correções finais

--- leitorsequencia.vhd
+++ leitorsequencia.vhd
@@ -1,7 +1,5 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity leitorsequencia is
     Port ( x : in STD_LOGIC_VECTOR(9 downto 0);
